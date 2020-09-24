423ddedb24f5e69d657e5ace3fc35dd556274038
Jim Talarski <jim.talarski@gmail.com>
2020-09-24 11:05:55 -0500

    pizza list first draft

---
+++ src/components/PizzaList/PizzaList.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import PizzaItem from '../compenent/PizzaItem';
+import {connect} from 'react-redux';
+
+class PizzaList extends Component {
+  render() {
+    return (
+      <section>
+        <h3>Pizza List</h3>
+        <ul>
+          {this.props.pizzaTest.map((pizza, i) => 
+            <PizzaItem 
+              id={pizza.id}
+              pizza={pizza.pizza}
+              price={pizza.price}
+              />
+          )}
+        </ul>
+      </section>
+    );
+  }
+} 
+
+const mapStateToProps = (reduxState) => {
+  return {
+    PizzaList: reduxState.PizzaList;
+  };
+}
+
+export default connect(mapStateToProps)(PizzaList);
